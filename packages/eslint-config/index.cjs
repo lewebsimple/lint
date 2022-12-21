83234d5eeea0fc520be1bffd4412a5721fb80c35
Pascal Martineau <pascal@lewebsimple.ca>
2022-12-21 15:59:08 -0500

    minor: use .cjs files instead of .js

---
+++ packages/eslint-config/index.cjs
@@ -0,0 +1,22 @@
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  rules: {
+    // Prettier
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 120,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: "all",
+      },
+    ],
+  },
+});
