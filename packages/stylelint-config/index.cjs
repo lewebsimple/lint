55884c7ea3fe7b1b89429c72fa877b647568e585
Pascal Martineau <pascal@lewebsimple.ca>
2022-12-22 09:54:38 -0500

    remove: no need for stylelint-config-prettier

--- packages/stylelint-config/index.cjs
+++ packages/stylelint-config/index.cjs
@@ -1,17 +1,3 @@
-const { resolve } = require("path");
-
 module.exports = {
-  configBaseDir: resolve(__dirname, "node_modules"),
-  extends: ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 120,
-        semi: true,
-        tabWidth: 2,
-        trailingComma: "all",
-      },
-    ],
-  },
+  extends: ["stylelint-config-standard-scss"],
 };
