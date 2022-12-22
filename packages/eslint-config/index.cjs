f18a76f62388664b5db6370824fa19030df260c7
Pascal Martineau <pascal@lewebsimple.ca>
2022-12-22 09:02:33 -0500

    feat: add typescript rules

--- packages/eslint-config/index.cjs
+++ packages/eslint-config/index.cjs
@@ -6,8 +6,29 @@ module.exports = defineConfig({
     browser: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ["plugin:@typescript-eslint/recommended", "eslint:recommended", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      files: ["*.js", "*.cjs"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
   rules: {
+    // TypeScript
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { fixStyle: "inline-type-imports", disallowTypeAnnotations: false },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-undef": "off",
+    "no-unused-vars": "off",
+
     // Prettier
     "prettier/prettier": [
       "error",
