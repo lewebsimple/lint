17afc8186ea06f3b0162112b340f67fc1afcf9ed
Pascal Martineau <pascal@lewebsimple.ca>
2022-12-22 10:56:22 -0500

    feat: stylelint-prettier and tailwind support

--- packages/stylelint-config/index.cjs
+++ packages/stylelint-config/index.cjs
@@ -1,3 +1,18 @@
+const ignoreAtRules = ["tailwind", "apply", "variants", "responsive", "screen"];
+
 module.exports = {
-  extends: ["stylelint-config-standard-scss"],
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-vue/scss",
+    "stylelint-prettier/recommended",
+  ],
+  plugins: ["stylelint-prettier"],
+  rules: {
+    // Tailwind CSS
+    "at-rule-no-unknown": [true, { ignoreAtRules }],
+    "scss/at-rule-no-unknown": [true, { ignoreAtRules }],
+    "no-descending-specificity": null,
+    // Prettier
+    "prettier/prettier": [true, { printWidth: 120, tabWidth: 2 }],
+  },
 };
