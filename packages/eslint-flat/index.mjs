949623f07a5da5ca3318c964e36587742dd84f07
Pascal Martineau <pascal@lewebsimple.ca>
2024-10-28 11:05:08 -0400

    feat: use @nuxt/eslint-config

--- packages/eslint-flat/index.mjs
+++ packages/eslint-flat/index.mjs
@@ -1,44 +1,14 @@
-import eslint from "@eslint/js";
-import stylistic from "@stylistic/eslint-plugin";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import globals from "globals";
-import tsEslint from "typescript-eslint";
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-export default tsEslint.config(
-  eslint.configs.recommended,
-  { ignores: ["node_modules/", "dist/"] },
-  {
-    languageOptions: {
-      globals: { ...globals.node },
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-      "@stylistic": stylistic,
-    },
-    rules: {
-      "simple-import-sort/exports": "error",
-      "simple-import-sort/imports": "error",
-    },
-  },
-  stylistic.configs.customize({
-    arrowParens: "always",
-    commaDangle: "always-multiline",
-    indent: 2,
-    quotes: "double",
-    semi: true,
-  }),
-  {
-    files: ["**/*.ts"],
-    plugins: { "@typescript-eslint": tsPlugin },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
-  },
-);
+export default createConfigForNuxt({
+  features: {
+    nuxt: false,
+    stylistic: {
+      arrowParens: true,
+      commaDangle: "always-multiline",
+      indent: 2,
+      quotes: "double",
+      semi: true,
+    }
+  }
+});
