d6f87d7cfe1c56f470c171973f5c97c2dc03c692
Pascal Martineau <pascal@lewebsimple.ca>
2025-01-28 13:09:15 -0500

    refactor: stylelint-prettier => @stylistic/stylelint-plugin

--- packages/stylelint-config/index.cjs
+++ packages/stylelint-config/index.cjs
@@ -2,21 +2,18 @@ const ignoreAtRules = ["tailwind", "apply", "variants", "responsive", "screen"];
 
 module.exports = {
   extends: ["stylelint-config-standard-scss", "stylelint-config-standard-scss", "stylelint-config-standard-vue/scss"],
-  plugins: ["stylelint-prettier"],
+  plugins: ["@stylistic/stylelint-plugin"],
   rules: {
     // Tailwind CSS
     "at-rule-no-unknown": [true, { ignoreAtRules }],
-    "scss/at-rule-no-unknown": [true, { ignoreAtRules }],
     "no-descending-specificity": null,
-    // Prettier
-    "prettier/prettier": [true, { tabWidth: 2 }],
-    "selector-class-pattern": null,
+    "scss/at-rule-no-unknown": [true, { ignoreAtRules }],
     // Other
     "at-rule-empty-line-before": null,
     "no-invalid-position-at-import-rule": null,
     "rule-empty-line-before": null,
-    "scss/operator-no-unspaced": null,
     "scss/operator-no-newline-after": null,
+    "scss/operator-no-unspaced": null,
   },
   ignoreFiles: ["**/*", "!**/*.{css,scss,vue}"],
 };
